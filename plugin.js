faa1772ffeeb1b31c3d0d239d3d91b1cd522232f
aminpaks <amin.pakseresht@hotmail.com>
2017-09-04 13:33:49 -0400

    Adds strong typings for Typescript

--- plugin.js
+++ plugin.js
@@ -1,51 +1,51 @@
 'use strict';
 /**
- * @license Rollup Plugin Absolute Module Fix v0.0.1
+ * @license Rollup Plugin Absolute Module Fix v0.0.2
  * (c) 2017 Amin Paks <amin.pakseresht@hotmail.com>
  * License: MIT
  */
-const MagicString = require('magic-string');
+var MagicString = require('magic-string');
 
-const importRegex = /(import|export)\s*((\{[^}]*\}|\*)(\s*as\s+\w+)?(\s+from)?\s*)?([`'"])(.*)\6/ig;
+var importRegex = /(import|export)\s*((\{[^}]*\}|\*)(\s*as\s+\w+)?(\s+from)?\s*)?([`'"])(.*)\6/ig;
 
 module.exports = function () {
 
     function fixImportee(id) {
-        const parts = id.split(/[\/\\]/);
-        const firstLetter = id.slice(0, 1);
-        const last = parts.pop();
+        var parts = id.split(/[\/\\]/);
+        var firstLetter = id.slice(0, 1);
+        var last = parts.pop();
 
         if (firstLetter !== '.' && last === 'index') {
             return parts.join('/');
         }
     }
 
-    const plugin = {
+    var plugin = {
         name: 'absolute-module-fix',
         transform: function (code) {
-            const magicString = new MagicString(code);
+            var magicString = new MagicString(code);
 
             let hasUpdate = false;
             let match;
 
             while ((match = importRegex.exec(code)) !== null) {
-                const moduleId = match[7];
-                const fix = fixImportee(moduleId);
+                var moduleId = match[7];
+                var fix = fixImportee(moduleId);
 
                 if (moduleId && fix) {
                     hasUpdate = true;
 
-                    const start = match.index;
-                    const end = start + match[0].length;
-                    const replacement = code.substring(start, end).replace(moduleId, fix);
+                    var start = match.index;
+                    var end = start + match[0].length;
+                    var replacement = code.substring(start, end).replace(moduleId, fix);
 
                     magicString.overwrite(start, end, replacement);
                 }
             }
 
             if (hasUpdate) {
-                const code = magicString.toString();
-                const map = magicString.generateMap({ hires: true });
+                var code = magicString.toString();
+                var map = magicString.generateMap({ hires: true });
 
                 return {
                     code,
